61a0aa3ccae810118d2499179fab8f69f62daf8e
nicolaurenti <nicolaurenti.94@gmail.com>
2022-08-15 23:08:07 -0300

    Splash screen added

--- app/src/main/java/com/onboarding/marvelapp/MainActivity.kt
+++ app/src/main/java/com/onboarding/marvelapp/MainActivity.kt
@@ -1,11 +1,24 @@
-package com.onboarding.marvelapp
+package com.example.marvelapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.onboarding.marvelapp.R
+import com.onboarding.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.marvelLogoSplashImageView.startAnimation(
+            AnimationUtils.loadAnimation(
+                this,
+                R.anim.vertical_slide
+            )
+        )
     }
 }
