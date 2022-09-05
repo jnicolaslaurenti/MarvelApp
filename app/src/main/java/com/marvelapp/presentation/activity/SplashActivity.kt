46f32035e706db89c908a5ecc746bb186689436f
nicolaurenti <nicolaurenti.94@gmail.com>
2022-09-05 17:03:16 -0300

    MVVM really fixed

--- app/src/main/java/com/marvelapp/presentation/activity/SplashActivity.kt
+++ app/src/main/java/com/marvelapp/presentation/activity/SplashActivity.kt
@@ -1,20 +1,15 @@
 package com.marvelapp.presentation.activity
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.marvelapp.R
-import com.marvelapp.data.MarvelStates
 import com.marvelapp.databinding.ActivitySplashBinding
 import com.marvelapp.presentation.viewModel.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -26,26 +21,21 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        splashViewModel.startAnimation()
-        binding.apply {
-            startAnimation()
+        lifecycleScope.launchWhenStarted {
+            splashViewModel.state.collectLatest { this@SplashActivity.updateUI(it) }
         }
+        splashViewModel.runAnimation()
     }
 
-    private fun startAnimation() {
-        val context = this
-        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.vertical_slide)
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                splashViewModel.state.collect {
-                    if (it.animation == MarvelStates.START_ANIMATION) {
-                        binding.marvelLogoSplashImageView.startAnimation(slideAnimation)
-                        Handler(Looper.getMainLooper()).postDelayed({
-                            MarvelActivity.launch(context)
-                            finish()
-                        }, 3000)
-                    }
-                }
+    private fun updateUI(state: SplashViewModel.UiState) {
+        when (state.animation) {
+            SplashViewModel.SplashStates.START_ANIMATION -> {
+                val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.vertical_slide)
+                binding.marvelLogoSplashImageView.startAnimation(slideAnimation)
+            }
+            SplashViewModel.SplashStates.FINISH_ANIMATION -> {
+                startActivity(MarvelActivity.getIntent(this))
+                finish()
             }
         }
     }
