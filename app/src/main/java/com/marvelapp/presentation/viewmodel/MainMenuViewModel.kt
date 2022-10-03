bc91056bffd5d07607a48581ddf10b347db5cf87
nicolaurenti <nicolaurenti.94@gmail.com>
2022-10-03 17:18:30 -0300

    UT added

---
+++ app/src/main/java/com/marvelapp/presentation/viewmodel/MainMenuViewModel.kt
@@ -0,0 +1,28 @@
+package com.marvelapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainMenuViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow(MainMenuStates.WAITING)
+    val state: StateFlow<MainMenuStates> = _state.asStateFlow()
+
+    fun goToCharacterScreen() =
+        viewModelScope.launch {
+            _state.update { MainMenuStates.BUTTON_PRESSED }
+        }
+
+    enum class MainMenuStates {
+        WAITING,
+        BUTTON_PRESSED,
+    }
+}
